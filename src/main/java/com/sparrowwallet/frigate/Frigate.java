5124333b145acc2a6385f5cd4aeb918c8c896cd7
Craig Raw <craigraw@gmail.com>
2025-11-14 15:58:14 +0200

    bump to v1.3.2

--- src/main/java/com/sparrowwallet/frigate/Frigate.java
+++ src/main/java/com/sparrowwallet/frigate/Frigate.java
@@ -18,7 +18,7 @@ import java.util.Locale;
 
 public class Frigate {
     public static final String SERVER_NAME = "Frigate";
-    public static final String SERVER_VERSION = "1.3.1";
+    public static final String SERVER_VERSION = "1.3.2";
     public static final String APP_HOME_PROPERTY = "frigate.home";
     public static final String NETWORK_ENV_PROPERTY = "FRIGATE_NETWORK";
     private static final int MAINNET_TAPROOT_ACTIVATION_HEIGHT = 709632;
